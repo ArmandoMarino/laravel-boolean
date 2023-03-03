9ffec8520f13b12ddaf9a3c657677f87cb228313
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-03-03 12:37:17 +0100

    controller and routes on home, init home html

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+@section('title', 'HOME')
+
+{{-- HERO INCLUDE --}}
+@section('content')
+<div class="container text-center">
+    <h1>Welcome on DEV MAGAZINE!</h1>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
-// Route::resource('tools', ToolController::class);
+Route::resource('tools', ToolController::class);
